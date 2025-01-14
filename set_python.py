88ef2b06e6c259d5df8fed5af34a68d56f91d0dd
neera3046 <55521328+neera3046@users.noreply.github.com>
2025-01-14 11:16:15 +0100

    Add files via upload

---
+++ set_python.py
@@ -0,0 +1,12 @@
+# nums={1,2,3,4}
+# set2={1,2,3,4,5,6,7}
+# print(set2)
+# print(set2.pop())
+# set2.add("neeraj")
+# print(set2)
+# empty_set=set()
+# empty_set.add(789)
+# print(empty_set)
+set1={1,2,3}
+set2={2,3,4}
+print(set1.intersection(set2))
